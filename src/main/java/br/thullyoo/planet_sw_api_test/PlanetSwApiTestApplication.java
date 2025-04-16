3434001587d240b687cf46418102ad110347e082
Thullyoo <thullyofilho@gmail.com>
2025-04-16 11:38:59 -0300

    Initial commit

---
+++ src/main/java/br/thullyoo/planet_sw_api_test/PlanetSwApiTestApplication.java
@@ -0,0 +1,13 @@
+package br.thullyoo.planet_sw_api_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetSwApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetSwApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/thullyoo/planet_sw_api_test/PlanetSwApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.planet_sw_api_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetSwApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
