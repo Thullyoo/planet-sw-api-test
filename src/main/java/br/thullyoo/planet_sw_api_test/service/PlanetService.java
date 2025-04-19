b6a6a07c7d7b76a15699220573b89a47000707c9
Thullyoo <thullyofilho@gmail.com>
2025-04-19 19:01:42 -0300

    feat: add method and endpoint to list planets

--- src/main/java/br/thullyoo/planet_sw_api_test/service/PlanetService.java
+++ src/main/java/br/thullyoo/planet_sw_api_test/service/PlanetService.java
@@ -1,9 +1,12 @@
 package br.thullyoo.planet_sw_api_test.service;
 
 import br.thullyoo.planet_sw_api_test.model.Planet;
+import br.thullyoo.planet_sw_api_test.model.builder.QueryBuilder;
 import br.thullyoo.planet_sw_api_test.repository.PlanetRepository;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -25,5 +28,10 @@ public class PlanetService {
 
     public Optional<Planet> getByName(String name) {
         return planetRepository.findByName(name);
+    }
+
+    public List<Planet> list(String terrain, String climate) {
+        Example<Planet> query = QueryBuilder.makeQuery(new Planet(climate, terrain));
+        return planetRepository.findAll(query);
     }
 }
