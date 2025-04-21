0f78db229ed1373cc8bc3935f417ef53a18496f2
Thullyoo <thullyofilho@gmail.com>
2025-04-21 14:03:40 -0300

    feat: add test integration on repository when create planet

---
+++ src/test/java/br/thullyoo/planet_sw_api_test/repository/PlanetRepositoryTest.java
@@ -0,0 +1,54 @@
+package br.thullyoo.planet_sw_api_test.repository;
+
+import br.thullyoo.planet_sw_api_test.model.Planet;
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.InvalidPropertyException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static br.thullyoo.planet_sw_api_test.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+@DataJpaTest
+class PlanetRepositoryTest {
+
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_withValidData_returnsPlanet(){
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet res = testEntityManager.find(Planet.class, planet.getId());
+
+        assertThat(res).isNotNull();
+        assertThat(res.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(res.getName()).isEqualTo(PLANET.getName());
+        assertThat(res.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+    @Test
+    public void createPlanet_withInvalidData_throwsException() {
+        Planet emptyPlanet = new Planet();
+        Planet invalidPlanet = new Planet("", "", "");
+
+        assertThatThrownBy(() -> planetRepository.save(emptyPlanet)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> planetRepository.save(invalidPlanet)).isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @Test
+    public void createPlanet_withExistingName_throwsException(){
+        Planet planet = testEntityManager.persistFlushFind(PLANET);
+        testEntityManager.detach(planet);
+        planet.setId(null);
+
+        assertThatThrownBy(() -> planetRepository.save(planet)).isInstanceOf(RuntimeException.class);
+    }
+}
